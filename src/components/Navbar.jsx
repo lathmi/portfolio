0d0d0e53af40257a9cbbb26d3e1e52972056685c
lathmi <lathmiis@gmail.com>
2026-06-11 04:39:50 +0530

    hero section completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,18 @@
 import { cn } from "@/lib/utils"
 import { useEffect, useState } from "react";
+import { Menu } from "lucide-react";
 
 const navItems = [
-    {name: "Home", href: ""},
-    {name: "About", href: ""},
-    {name: "Skills", href: ""},
-    {name: "Projects", href: ""},
-    {name: "Contacts", href: ""},
+    {name: "Home", href: "#hero"},
+    {name: "About", href: "#about"},
+    {name: "Skills", href: "#skills"},
+    {name: "Projects", href: "#projects"},
+    {name: "Contacts", href: "#contact"},
 ];
 
 export const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -37,6 +39,52 @@ export const Navbar = () => {
                         Portfolio
                     </span>
                 </a>
+
+            {/* desktop nav */}
+
+                <div className="hidden md:flex space-x-8 ">
+                    {navItems.map((item,key) => (
+
+                        <a key={key} href={item.href} className="text-foreground/80 hover:text-primary transition-colors duration-300">
+                            {item.name}
+                        </a>
+                    ))}
+                </div>
+
+            {/* mobile nav */}
+
+            <button 
+                onClick={() => setIsMenuOpen((prev) => !prev)}
+                className="md:hidden p-2 text-foreground z-50"
+                aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
+
+            > 
+                {isMenuOpen ? <X size={24} /> : <Menu size={24}/>}
+                
+            </button>
+
+                <div className={cn(
+                    "fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center",
+                    "transition-all duration-300 md:hidden",
+                    isMenuOpen 
+                    ? "opacity-100 pointer-events-auto" 
+                    : "opacity-0 pointer-events-none"
+                    )}
+                >
+                    <div className="flex flex-col space-y-8 text-xl">
+                        {navItems.map((item,key) => (
+
+                            <a 
+                                key={item.name} 
+                                href={item.href} 
+                                className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                {item.name}
+                            </a>
+                        ))}
+                    </div>
+                </div>
             </div>
         </nav>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import {StarBackground} from "@/components/StarBackground";
 import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
 
 export const Home = () => {
 
@@ -15,6 +16,9 @@ export const Home = () => {
         <Navbar/>
 
         {/* Main content */}
+        <main>
+            <HeroSection/>
+        </main>
 
 
         {/* Footer */}
